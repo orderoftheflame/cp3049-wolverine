caaeaaf17408e863b90f009951576fe1edcb5793
noirenex <noirenex@851e1a0a-1b00-11df-97b5-87196d179026>
2010-03-13 12:34:10 +0000

    Added meeting Day/Time to student group, consideration to whether group will meet more than once a week needs to be made, as architecture will have to change if required.

--- trunk/Website/classes/class.studentgroup.php
+++ trunk/Website/classes/class.studentgroup.php
@@ -4,13 +4,17 @@ class StudentGroup{
 protected $groupID;
 protected $groupTitle;
 protected $details;
+protected $day;
+protected $time;
 protected $studentsCollection;
-
-public static function withParameters($groupTitle, $details)
+//QUERY: Will a group ever meet more than once a week? In that case we need another table to store meeting days/times.
+public static function withParameters($groupTitle, $details, $day, $time)
 {
 	$group = new StudentGroup();
 	$group->setTitle($groupTitle);
 	$group->setDetails($details);
+	$group->setDay($day);
+	$group->setTime($time);
 	return $project;
 }
 
@@ -18,24 +22,28 @@ public static function withParameters($groupTitle, $details)
 public static function fromRow($row){
 	$groupID = $row['IntGroupID'];
 	$title = $row['VchGroupTitle'];
-	$details = $row['vchGroupDetails'];
+	$details = $row['VchGroupDetails'];
+	$day = $row['VchGroupDay'];
+	$time = $row['TimeGroupMeeting'];
 	
-	$group = StudentGroup::withParameters($title, $details);
+	$group = StudentGroup::withParameters($title, $details, $day, $time);
 	$group->setGroupID($groupID);
 	return $group;
 }
 
 public function save(){
-  $sql = "INSERT INTO wv_studentgroups (VchGroupTitle, VchGroupDetails, DateTimeCreated)
+  $sql = "INSERT INTO wv_studentgroups (VchGroupTitle, VchGroupDetails, VchGroupDay, TimeGroupMeeting, DateTimeCreated)
   VALUES (
     ".$this->getTitle().",
     ".$this->getDetails().",
+	".$this->getDay().",
+	".$this->getTime().",
     ".date('Y-m-d H:i:s')."
   )";
 }
 
 //Getters and setters
-public function setGrouptID($value){
+public function setGroupID($value){
 	$this->groupID = $value;
 }
 public function setTitle($value){
@@ -43,6 +51,12 @@ public function setTitle($value){
 }
 public function setDetails($value){
 	$this->details = $value;
+}
+public function setDay($value){
+	$this->day = $value;
+}
+public function setTime($value){
+	$this->time = $value;
 }
 
 public function getGroupID(){
@@ -53,6 +67,12 @@ public function getTitle(){
 }
 public function getDetails(){
 	return $this->details;
+}
+public function setDay(){
+	return $this->day;
+}
+public function setTime(){
+	return $this->time;
 }
 }
 ?>
